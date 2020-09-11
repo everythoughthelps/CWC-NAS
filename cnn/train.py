7fcae5480aeb0281fa3befb88cbeb3d46fae9e33
panmeng <panm9@mail2.sysu.edu.cn>
2020-09-11 16:57:53 +0800

    revert train.py

--- cnn/train.py
+++ cnn/train.py
@@ -8,20 +8,21 @@ import utils
 import logging
 import argparse
 import torch.nn as nn
-from kd_loss import KDLoss
-from torch.utils.data import DataLoader
+import genotypes
+import torch.utils
+import torchvision.datasets as dset
 import torch.backends.cudnn as cudnn
-import dataset
+
+from torch.autograd import Variable
 from model import NetworkCIFAR as Network
 
 
 parser = argparse.ArgumentParser("cifar")
-parser.add_argument('--dataroot', type=str, default='../data', help='location of the data corpus')
+parser.add_argument('--data', type=str, default='../data', help='location of the data corpus')
 parser.add_argument('--batch_size', type=int, default=96, help='batch size')
 parser.add_argument('--learning_rate', type=float, default=0.025, help='init learning rate')
 parser.add_argument('--momentum', type=float, default=0.9, help='momentum')
 parser.add_argument('--weight_decay', type=float, default=3e-4, help='weight decay')
-parser.add_argument('--cls', '-cls', action='store_true', help='adding cls loss')
 parser.add_argument('--report_freq', type=float, default=50, help='report frequency')
 parser.add_argument('--gpu', type=int, default=0, help='gpu device id')
 parser.add_argument('--epochs', type=int, default=600, help='num of training epochs')
@@ -37,7 +38,6 @@ parser.add_argument('--save', type=str, default='EXP', help='experiment name')
 parser.add_argument('--seed', type=int, default=0, help='random seed')
 parser.add_argument('--arch', type=str, default='DARTS', help='which architecture to use')
 parser.add_argument('--grad_clip', type=float, default=5, help='gradient clipping')
-parser.add_argument('--temp', default=4.0, type=float, help='temperature scaling')
 args = parser.parse_args()
 
 args.save = 'eval-{}-{}'.format(args.save, time.strftime("%Y%m%d-%H%M%S"))
@@ -51,6 +51,7 @@ fh.setFormatter(logging.Formatter(log_format))
 logging.getLogger().addHandler(fh)
 
 CIFAR_CLASSES = 10
+
 
 def main():
   if not torch.cuda.is_available():
@@ -80,83 +81,18 @@ def main():
       momentum=args.momentum,
       weight_decay=args.weight_decay
       )
-  kdloss = KDLoss(args.temp)
 
-  if not args.cls:
-    train_queue, valid_queue= dataset.load_dataset(args.dataset, args.dataroot, batch_size=args.batch_size)
-  else:
-    train_queue, valid_queue= dataset.load_dataset(args.dataset, args.dataroot, 'pair', batch_size=args.batch_size)
+  train_transform, valid_transform = utils._data_transforms_cifar10(args)
+  train_data = dset.CIFAR10(root=args.data, train=True, download=True, transform=train_transform)
+  valid_data = dset.CIFAR10(root=args.data, train=False, download=True, transform=valid_transform)
+
+  train_queue = torch.utils.data.DataLoader(
+      train_data, batch_size=args.batch_size, shuffle=True, pin_memory=True, num_workers=2)
+
+  valid_queue = torch.utils.data.DataLoader(
+      valid_data, batch_size=args.batch_size, shuffle=False, pin_memory=True, num_workers=2)
 
   scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, float(args.epochs))
-
-  def train(train_queue, model, criterion, optimizer):
-    train_cls_loss = 0
-    objs = utils.AvgrageMeter()
-    top1 = utils.AvgrageMeter()
-    top5 = utils.AvgrageMeter()
-    model.train()
-
-    for step, (input, target) in enumerate(train_queue):
-      batch_size = input.size(0)
-      input = input.cuda()
-      target = target.cuda(async=True)
-
-      optimizer.zero_grad()
-      if not args.cls:
-        logits, logits_aux = model(input)
-        loss = criterion(logits, target)
-        if args.auxiliary:
-          loss_aux = criterion(logits_aux, target)
-          loss += args.auxiliary_weight * loss_aux
-      else:
-        targets_ = target[:batch_size // 2]
-        logits, logits_aux = model(input[:batch_size // 2])
-        loss = torch.mean(criterion(logits, targets_))
-        loss += loss.item()
-
-        with torch.no_grad():
-          outputs_cls, _ = model(input[batch_size // 2:])
-        cls_loss = kdloss(logits, outputs_cls.detach())
-        loss += args.lamda * cls_loss
-        train_cls_loss += cls_loss.item()
-      loss.backward()
-      nn.utils.clip_grad_norm(model.parameters(), args.grad_clip)
-      optimizer.step()
-
-      prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-      n = input.size(0)
-      objs.update(loss.data[0], n)
-      top1.update(prec1.data[0], n)
-      top5.update(prec5.data[0], n)
-
-      if step % args.report_freq == 0:
-        logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
-
-    return top1.avg, objs.avg
-
-  def infer(valid_queue, model, criterion):
-    objs = utils.AvgrageMeter()
-    top1 = utils.AvgrageMeter()
-    top5 = utils.AvgrageMeter()
-    model.eval()
-
-    for step, (input, target) in enumerate(valid_queue):
-      input = input.cuda()
-      target = target.cuda(async=True)
-
-      logits, _ = model(input)
-      loss = criterion(logits, target)
-
-      prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
-      n = input.size(0)
-      objs.update(loss.data[0], n)
-      top1.update(prec1.data[0], n)
-      top5.update(prec5.data[0], n)
-
-      if step % args.report_freq == 0:
-        logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
-
-    return top1.avg, objs.avg
 
   for epoch in range(args.epochs):
     scheduler.step()
@@ -170,6 +106,63 @@ def main():
     logging.info('valid_acc %f', valid_acc)
 
     utils.save(model, os.path.join(args.save, 'weights.pt'))
+
+
+def train(train_queue, model, criterion, optimizer):
+  objs = utils.AvgrageMeter()
+  top1 = utils.AvgrageMeter()
+  top5 = utils.AvgrageMeter()
+  model.train()
+
+  for step, (input, target) in enumerate(train_queue):
+    input = Variable(input).cuda()
+    target = Variable(target).cuda(async=True)
+
+    optimizer.zero_grad()
+    logits, logits_aux = model(input)
+    loss = criterion(logits, target)
+    if args.auxiliary:
+      loss_aux = criterion(logits_aux, target)
+      loss += args.auxiliary_weight*loss_aux
+    loss.backward()
+    nn.utils.clip_grad_norm(model.parameters(), args.grad_clip)
+    optimizer.step()
+
+    prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
+    n = input.size(0)
+    objs.update(loss.data[0], n)
+    top1.update(prec1.data[0], n)
+    top5.update(prec5.data[0], n)
+
+    if step % args.report_freq == 0:
+      logging.info('train %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+
+  return top1.avg, objs.avg
+
+
+def infer(valid_queue, model, criterion):
+  objs = utils.AvgrageMeter()
+  top1 = utils.AvgrageMeter()
+  top5 = utils.AvgrageMeter()
+  model.eval()
+
+  for step, (input, target) in enumerate(valid_queue):
+    input = Variable(input, volatile=True).cuda()
+    target = Variable(target, volatile=True).cuda(async=True)
+
+    logits, _ = model(input)
+    loss = criterion(logits, target)
+
+    prec1, prec5 = utils.accuracy(logits, target, topk=(1, 5))
+    n = input.size(0)
+    objs.update(loss.data[0], n)
+    top1.update(prec1.data[0], n)
+    top5.update(prec5.data[0], n)
+
+    if step % args.report_freq == 0:
+      logging.info('valid %03d %e %f %f', step, objs.avg, top1.avg, top5.avg)
+
+  return top1.avg, objs.avg
 
 
 if __name__ == '__main__':
